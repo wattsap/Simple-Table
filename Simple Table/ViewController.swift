7236215f37ccd4317f6f745c37d3398cab7ffb6f
Andrew Watts <wattsap@appstate.edu>
2015-06-09 09:59:44 -0400

    Initial Commit

---
+++ Simple Table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Table
+//
+//  Created by Andrew Watts on 6/9/15.
+//  Copyright (c) 2015 Andrew Watts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
